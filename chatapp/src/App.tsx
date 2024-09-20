ad08f7503a94d56369fcfb1a206d9bfc74043b28
rohan <rohanwagh234@gmail.com>
2024-09-20 09:59:58 +0530

    card added

--- chatapp/src/App.tsx
+++ chatapp/src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "./Components/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card className="border-2 h-full min-h-32 w-3/4 flex justify-center items-end m-auto pb-2">
+        <div className="relative w-full p-4">
+          <textarea
+            placeholder="write a message..."
+            rows={5}
+            cols={10}
+            className="font-mono resize-none bg-gray-100 outline-none w-full p-4 pr-10 border rounded-2xl"
+          />
+          <i className="absolute bottom-10 right-12 text-xl text-gray-500">
+            hi
+          </i>
+        </div>
+      </Card>
     </div>
   );
 }
